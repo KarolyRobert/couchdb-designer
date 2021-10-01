27045bad298771f234caba0b0d8cb19b8921f34d
RoGeR <karoly.robert.das@gmail.com>
2021-10-01 21:31:13 +0200

    export

--- build/index.js
+++ build/index.js
@@ -6,10 +6,13 @@ var _createDeisgnDocument = _interopRequireDefault(require("./createDeisgnDocume
 
 var _createTestContext = _interopRequireDefault(require("./createTestContext"));
 
+var _createTestServer = _interopRequireDefault(require("./createTestServer"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = {
   designer: _designer.default,
   createDesignDocument: _createDeisgnDocument.default,
-  createTestContext: _createTestContext.default
+  createTestContext: _createTestContext.default,
+  createTestServer: _createTestServer.default
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import designer from './designer';
 import createDesignDocument from './createDeisgnDocument';
 import createTestContext from './createTestContext';
+import createTestServer from './createTestServer';
 
-module.exports = { designer, createDesignDocument, createTestContext };
+module.exports = { designer, createDesignDocument, createTestContext, createTestServer };
