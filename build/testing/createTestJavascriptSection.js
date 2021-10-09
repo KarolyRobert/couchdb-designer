3ec7f691163186de8e01a3e098a87a03a3c2905c
RoGeR <karoly.robert.das@gmail.com>
2021-10-09 13:08:12 +0200

    eval exception handler

--- build/testing/createTestJavascriptSection.js
+++ build/testing/createTestJavascriptSection.js
@@ -31,36 +31,40 @@ const createTestJavascriptModule = (contextId, fileContent) => {
 
 const createTestJavascriptSection = (fileStats, contextProps, fileContent) => {
   return new Promise((resolve, reject) => {
-    const testModule = createTestJavascriptModule(contextProps.contextId, fileContent);
+    try {
+      const testModule = createTestJavascriptModule(contextProps.contextId, fileContent);
 
-    if (testModule && Object.keys(testModule).length > 0) {
-      let testModuleKeys = Object.keys(testModule);
+      if (testModule && Object.keys(testModule).length > 0) {
+        let testModuleKeys = Object.keys(testModule);
 
-      if (!fileStats.isLib && testModuleKeys.length === 1 && testModuleKeys[0] === fileStats.name) {
-        const mockFunction = (0, _createMockFunction.default)(fileStats, contextProps, testModuleKeys[0], testModule[testModuleKeys[0]]);
-        resolve({
-          [fileStats.name]: mockFunction
-        });
-      } else {
-        let testElementsObject = {
-          __sourceProperties__: fileStats
-        };
+        if (!fileStats.isLib && testModuleKeys.length === 1 && testModuleKeys[0] === fileStats.name) {
+          const mockFunction = (0, _createMockFunction.default)(fileStats, contextProps, testModuleKeys[0], testModule[testModuleKeys[0]]);
+          resolve({
+            [fileStats.name]: mockFunction
+          });
+        } else {
+          let testElementsObject = {
+            __sourceProperties__: fileStats
+          };
 
-        for (let moduleElementName of testModuleKeys) {
-          if (typeof testModule[moduleElementName] === 'function') {
-            const mockFunction = (0, _createMockFunction.default)(fileStats, contextProps, moduleElementName, testModule[moduleElementName]);
-            testElementsObject[moduleElementName] = mockFunction;
-          } else {
-            testElementsObject[moduleElementName] = testModule[moduleElementName];
+          for (let moduleElementName of testModuleKeys) {
+            if (typeof testModule[moduleElementName] === 'function') {
+              const mockFunction = (0, _createMockFunction.default)(fileStats, contextProps, moduleElementName, testModule[moduleElementName]);
+              testElementsObject[moduleElementName] = mockFunction;
+            } else {
+              testElementsObject[moduleElementName] = testModule[moduleElementName];
+            }
           }
-        }
 
-        resolve({
-          [fileStats.name]: testElementsObject
-        });
+          resolve({
+            [fileStats.name]: testElementsObject
+          });
+        }
+      } else {
+        reject(`The module ${fileStats.filePath} does not exist't export anything! You must export function/s with module.exports = {...}`);
       }
-    } else {
-      reject(`The module ${fileStats.filePath} does not exist't export anything! You must export function/s with module.exports = {...}`);
+    } catch (moduleError) {
+      reject(`Can not evaluate ${fileStats.filePath}\n ${moduleError.message}`);
     }
   });
 };
--- src/testing/createTestJavascriptSection.js
+++ src/testing/createTestJavascriptSection.js
@@ -6,34 +6,38 @@ const createTestJavascriptModule = (contextId,fileContent) => {
     const {require,emit,log,sum,getRow,provides,registerType,start,send,index} = testEnvironment(contextId);
     const toJSON = JSON.stringify;
     const isArray = Array.isArray;
-    return eval(fileContent);
+    return eval(fileContent); 
 }
 
 
 const createTestJavascriptSection = (fileStats, contextProps, fileContent) => {
     return new Promise((resolve, reject) => {
-        const testModule = createTestJavascriptModule(contextProps.contextId, fileContent);
-        if(testModule && Object.keys(testModule).length > 0){
-            let testModuleKeys = Object.keys(testModule);
-            if(!fileStats.isLib && testModuleKeys.length === 1 && testModuleKeys[0] === fileStats.name){
-                
-                const mockFunction = createMockFunction(fileStats, contextProps, testModuleKeys[0], testModule[testModuleKeys[0]]);
-                resolve({[fileStats.name]:mockFunction});
-            }else{
-                let testElementsObject = {__sourceProperties__:fileStats};
-                for(let moduleElementName of testModuleKeys){
-                    if(typeof testModule[moduleElementName] === 'function'){
-                        const mockFunction = createMockFunction(fileStats,contextProps,moduleElementName,testModule[moduleElementName]);
-                        testElementsObject[moduleElementName] = mockFunction;
-                    }else{
-                        testElementsObject[moduleElementName] = testModule[moduleElementName];
+        try{
+            const testModule = createTestJavascriptModule(contextProps.contextId, fileContent);
+            if(testModule && Object.keys(testModule).length > 0){
+                let testModuleKeys = Object.keys(testModule);
+                if(!fileStats.isLib && testModuleKeys.length === 1 && testModuleKeys[0] === fileStats.name){
+                    
+                    const mockFunction = createMockFunction(fileStats, contextProps, testModuleKeys[0], testModule[testModuleKeys[0]]);
+                    resolve({[fileStats.name]:mockFunction});
+                }else{
+                    let testElementsObject = {__sourceProperties__:fileStats};
+                    for(let moduleElementName of testModuleKeys){
+                        if(typeof testModule[moduleElementName] === 'function'){
+                            const mockFunction = createMockFunction(fileStats,contextProps,moduleElementName,testModule[moduleElementName]);
+                            testElementsObject[moduleElementName] = mockFunction;
+                        }else{
+                            testElementsObject[moduleElementName] = testModule[moduleElementName];
+                        }
                     }
-                }
-                resolve({[fileStats.name]:testElementsObject});
-            }   
-        }else{
-            reject(`The module ${fileStats.filePath} does not exist't export anything! You must export function/s with module.exports = {...}`);
-        }  
+                    resolve({[fileStats.name]:testElementsObject});
+                }   
+            }else{
+                reject(`The module ${fileStats.filePath} does not exist't export anything! You must export function/s with module.exports = {...}`);
+            }
+        }catch(moduleError){
+            reject(`Can not evaluate ${fileStats.filePath}\n ${moduleError.message}`);
+        }
     });
 }
 
